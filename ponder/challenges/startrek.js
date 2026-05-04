64ef310fb0a80f293289b5dc70ab8b987a96567c
Maxwell0613-user <traviscm0613@gmail.com>
2026-05-04 14:37:13 -0600

    unhelpful challenge

---
+++ ponder/challenges/startrek.js
@@ -0,0 +1,9 @@
+
+document.getElementById('intro').style.backgroundColor = #f2c556;
+document.getElementById('em').style.backgroundColor = blue;
+
+
+let starship_img = document.createElement('img');
+starship_img.setAttribute('src','https://bit.ly/3RfG4sY' )
+let img_div = document.querySelector('#starship');
+img_div.appendChild = starship_img;
