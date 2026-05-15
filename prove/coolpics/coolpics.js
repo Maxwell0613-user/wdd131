437c7aac9088ed00f232c5623e0c005f4b66981d
Maxwell0613-user <traviscm0613@gmail.com>
2026-05-14 19:02:03 -0600

    made pop up images and a drop down bar

---
+++ prove/coolpics/coolpics.js
@@ -0,0 +1,49 @@
+const menu = document.querySelector('nav');
+const btn = document.querySelector('.menu-btn');
+
+btn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    menu.classList.toggle('menu');
+    btn.classList.toggle('change');
+}
+
+const gallery = document.querySelector('.photos');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+
+// Event listener for opening the modal
+gallery.addEventListener('click', openModal);
+
+function openModal(e) {
+    
+console.log(e.target);
+
+const img = e.target;
+
+const src = img.getAttribute('src');
+const alt = img.getAttribute('alt');
+
+const full = src.replace('sm', 'full');
+
+modalImage.src = full;
+modalImage.alt = alt;
+
+modal.showModal();
+
+// Code to show modal  - Use event parameter 'e'   
+    
+}
+// Close modal on button click
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+
+// Close modal if clicking outside the image
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
+});
+          
